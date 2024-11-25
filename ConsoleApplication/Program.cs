f7a1d2c8e570923e5f4a9efda3e1652b87bfb404
Prateek Gangopadhyay <Prateek.Gangopadhyay@intel.com>
2024-11-25 15:35:42 +0530

    Removed the project

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.Extensions.Logging;
+
+using ILoggerFactory factory = LoggerFactory.Create(builder =>
+{
+    builder.SetMinimumLevel(LogLevel.Trace);
+});
+ILogger logger = factory.CreateLogger("Program");
+
+Console.WriteLine("Hello, World!");
+MyMethod();
+
+void MyMethod()
+{
+    logger.LogError("Hello World! Logging is {Description}.", "fun");
+}
